7e79d3a8e7e01343da05a7677c313e2d835fbee6
Reena Mendonsa <reenamendonsa@gmail.com>
2022-06-07 19:02:50 +0530

    error boundary

---
+++ block-BRaadi/code/src/components/ErrorBoundary2.js
@@ -0,0 +1,22 @@
+import React from "react";
+class ErrorBoundary2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    // Update state so the next render will show the fallback UI.
+    return { hasError: true };
+  }
+
+  render() {
+    if (this.state.hasError) {
+      // You can render any custom fallback UI
+      return <h1>Something went wrong! Reload the page</h1>;
+    }
+
+    return this.props.children;
+  }
+}
+export default ErrorBoundary2;
--- block-BRaadi/code/src/index.js
+++ block-BRaadi/code/src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./style/app.css";
 import App from "./components/App";
+import ErrorBoundary2 from "./components/ErrorBoundary2";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ErrorBoundary2>
+      <App />
+    </ErrorBoundary2>
   </React.StrictMode>,
   document.getElementById("root")
 );
